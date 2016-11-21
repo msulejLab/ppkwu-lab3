0015c8ae8fc25d488face8ca3ec90ed0e1f095d1
Maciej Sulej <187809@edu.p.lodz.pl>
2016-11-22 00:17:44 +0100

    obsluga biblioteki http-zip w kliencie

--- src/main/java/pl/ppkwu/lab/App.java
+++ src/main/java/pl/ppkwu/lab/App.java
@@ -9,22 +9,25 @@ import pl.ppkwu.lab.impl.EncryptionLibraryImpl;
 import pl.ppkwu.lab.impl.HttpZipLibraryImpl;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class App {
 
     private static final String KEY = "ABCDEFghijkl1234";
-    private static final String MENU =   "1 - Szyfrowanie\n2 - Deszyfrowanie\n3 - Suma kontrolna";
 
     private static EncryptionLibrary encryptionLibrary;
+    private static HttpZipLibrary httpZipLibrary;
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
         encryptionLibrary = new EncryptionLibraryImpl(new EncryptionKey(KEY));
+        httpZipLibrary = new HttpZipLibraryImpl();
 
         do {
             System.out.println("Rodzaj operacji");
-            System.out.println(MENU);
+            System.out.println(menu());
             String operation = input("");
             
             switch (operation) {
@@ -37,10 +40,41 @@ public class App {
                 case "3":
                     handleChecksumFile();
                     break;
+                case "4":
+                    handleZipFile();
+                    break;
+                case "5":
+                    handleZipDirectory();
+                    break;
+                case "6":
+                    handleUnzipFile();
+                    break;
+                case "7":
+                    handleDownloadFile();
+                    break;
                 default:
                     System.out.println("Nieznana operacja");
             }
         } while (!input("Kontynuowac? [t/n]").equalsIgnoreCase("n"));
+    }
+
+    private static void handleDownloadFile() {
+        System.out.println("Wybrano pobieranie pliku");
+        String url = input("Adres pliku / strony");
+        if (!url.toLowerCase().contains("http://")) {
+            url = "http://" + url;
+        }
+
+        String outputFile = input("Plik wyjsciowy");
+
+        URL urlAddress = null;
+        try {
+            urlAddress = new URL(url);
+        } catch (MalformedURLException e) {
+            System.out.println("Podano bledny url");
+        }
+
+        httpZipLibrary.downloadFile(urlAddress, new File(outputFile));
     }
 
     private static void handleChecksumFile() {
@@ -67,10 +101,41 @@ public class App {
         encryptionLibrary.encryptFile(new File(inputFile), new File(outputFile));
     }
 
+    private static void handleZipDirectory() {
+        System.out.println("Wybrano kompresje folderu");
+        String inputDirectory = input("Folder do skompresowania");
+        String outputFile = input("Plik wyjsciowy (.zip)");
+        httpZipLibrary.unzipFile(new File(inputDirectory), new File(outputFile));
+    }
+
+    private static void handleZipFile() {
+        System.out.println("Wybrano kompresje pliku");
+        String inputFile = input("Plik do skompresowania");
+        String outputFile = input("Plik wyjsciowy (.zip)");
+        httpZipLibrary.zipFile(new File(inputFile), new File(outputFile));
+    }
+
+    private static void handleUnzipFile() {
+        System.out.println("Wybrano dekompresje pliku");
+        String inputFile = input("Plik wejsciowy");
+        String outputDirectory = input("Folder wyjsciowy");
+        httpZipLibrary.unzipFile(new File(inputFile), new File(outputDirectory));
+    }
+
     private static String input(String name) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("(" + name + "):: ");
+        System.out.print("(" + name + "):: ");
         String input = scanner.nextLine();
         return input;
+    }
+
+    private static String menu() {
+        return  "1 - Szyfrowanie\n" +
+                "2 - Deszyfrowanie\n" +
+                "3 - Suma kontrolna\n" +
+                "4 - Kompresja pliku\n" +
+                "5 - Kompresja folderu\n" +
+                "6 - Dekompresja pliku\n" +
+                "7 - Pobranie pliku (http)";
     }
 }
